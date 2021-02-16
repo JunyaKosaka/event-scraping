59fbb31128a526168e5e8d5d1d274b418065fe1b
Mighty-Fine <35888224+JunyaKosaka@users.noreply.github.com>
2021-02-16 11:43:12 +0900

    Add files via upload

--- eventScraping.py
+++ eventScraping.py
@@ -1,3 +1,32 @@
+import os
+import sys
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+from linebot.exceptions import (
+    LineBotApiError, InvalidSignatureError
+)
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    logger.error('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
 import json
 import requests
 
@@ -22,3 +51,32 @@ for e in event_data['events']:
 
 datas = sorted(datas, key=lambda d: d['startAt'])
 print(datas)
+
+def lambda_handler(event, context):
+    signature = event["headers"]["X-Line-Signature"]
+    body = event["body"]
+    ok_json = {"isBase64Encoded": False,
+               "statusCode": 200,
+               "headers": {},
+               "body": ""}
+    error_json = {"isBase64Encoded": False,
+                  "statusCode": 403,
+                  "headers": {},
+                  "body": "Error"}
+
+    @handler.add(MessageEvent, message=TextMessage)
+    def message(line_event):
+        text = line_event.message.text
+        line_bot_api.reply_message(line_event.reply_token, TextSendMessage(text=datas))
+
+    try:
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        logger.error("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            logger.error("  %s: %s" % (m.property, m.message))
+        return error_json
+    except InvalidSignatureError:
+        return error_json
+
+    return ok_json
